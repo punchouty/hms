1a630bb0089e8a03178953fbba2e65ddb44ac821
SK <sk@gmail.com>
2014-06-20 12:39:00 +0530

    Add Nimble

---
+++ hms/grails-app/domain/com/sapient/hms/security/Profile.groovy
@@ -0,0 +1,8 @@
+
+package com.sapient.hms.security
+
+class Profile extends grails.plugin.nimble.core.ProfileBase {
+
+	// Extend ProfileBase with your custom values here
+
+}
---
+++ hms/grails-app/domain/com/sapient/hms/security/User.groovy
@@ -0,0 +1,8 @@
+
+package com.sapient.hms.security
+
+class User extends grails.plugin.nimble.core.UserBase {
+
+	// Extend UserBase with your custom values here
+
+}
