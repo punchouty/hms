1557fc9ea057226b56c4f77dbd1ec02ef5aafffa
SK <sk@gmail.com>
2014-06-23 15:19:38 +0530

    Update

--- hms/grails-app/conf/BuildConfig.groovy
+++ hms/grails-app/conf/BuildConfig.groovy
@@ -55,10 +55,7 @@ grails.project.dependency.resolution = {
         //runtime ":yui-minify-resources:0.1.5"
 
         build ":tomcat:$grailsVersion"
-
-        runtime ":database-migration:1.3.2"
-
-        compile ':cache:1.0.1'
 		compile ':nimble:0.7'
+		
     }
 }
--- hms/grails-app/conf/SecurityFilters.groovy
+++ hms/grails-app/conf/SecurityFilters.groovy
@@ -1,9 +1,87 @@
 class SecurityFilters {
    def filters = {
-       loginCheck(controller: '*', action: '*') {
-           before = {
-			   
-           }
-       }
+	   secure(controller:"HiringController", action:"schedule") {
+		   before = {
+			   accessControl {
+				   role("HUMANRESOURCES") || permission("interview:schedule")
+			   }
+		   }
+		 }
+	   
+	   secure(controller:"HiringController", action:"delete") {
+		   before = {
+			   accessControl {
+				   role("HUMANRESOURCES") || permission("interview:delete")
+			   }
+		   }
+		 }
+	   
+	   secure(controller:"HiringController", action:"cancel") {
+		   before = {
+			   accessControl {
+				   role("HUMANRESOURCES") || permission("interview:cancel")
+			   }
+		   }
+		 }
+	   
+	   secure(controller:"HiringController", action:"reschedule") {
+		   before = {
+			   accessControl {
+				   role("HUMANRESOURCES") || permission("interview:reschedule")
+			   }
+		   }
+		 }
+	   
+	   secure(controller:"AssessmentController", action:"save") {
+		   before = {
+			   accessControl {
+				   role("HUMANRESOURCES") || permission("interview:round:save:results")
+			   }
+		   }
+		 }
+	   
+	   secure(controller:"AssessmentController", action:"requestReschedule") {
+		   before = {
+			   accessControl {
+				   role("HUMANRESOURCES") || permission("interview:round:request:reschedule")
+			   }
+		   }
+		 }
+	   secure(controller:"AssessmentController", action:"requestCancel") {
+		   before = {
+			   accessControl {
+				   role("HUMANRESOURCES") || permission("interview:round:request:cancel")
+			   }
+		   }
+		 }
+	   secure(controller:"AssessmentController", action:"markComplete") {
+		   before = {
+			   accessControl {
+				   role("HUMANRESOURCES") || permission("interview:round:complete")
+			   }
+		   }
+		 }
+	   secure(controller:"AssessmentController", action:"markStart") {
+		   before = {
+			   accessControl {
+				   role("HUMANRESOURCES") || permission("interview:round:start")
+			   }
+		   }
+		 }
+	   secure(controller:"AssessmentController", action:"requestAnotherRound") {
+		   before = {
+			   accessControl {
+				   role("HUMANRESOURCES") || permission("interview:round:request")
+			   }
+		   }
+		 }
+
+	   secure(controller:"AssessmentController", action:"updateResults") {
+		   before = {
+			   accessControl {
+				   role("HUMANRESOURCES") || permission("interview:round:update:results")
+			   }
+		   }
+		 }
    }
 }
