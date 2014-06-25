8d0bed02f93140e602685d14c8bc9465d39bbdc0
aku211 <aku211@dluaku2113762.sapient.com>
2014-06-25 10:49:38 +0530

    ADD

---
+++ hms/test/unit/com/sapient/hms/domain/InterviewDetailsControllerTests.groovy
@@ -0,0 +1,17 @@
+package com.sapient.hms.domain
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+//@TestFor(InterviewDetailsController)
+class InterviewDetailsControllerTests {
+
+    void testSample(){
+		assert 1==1
+	}
+}
---
+++ hms/test/unit/com/sapient/hms/domain/InterviewDetailsServiceTests.groovy
@@ -0,0 +1,25 @@
+
+package com.sapient.hms.domain
+
+import grails.test.mixin.*
+
+import org.junit.*
+
+import com.sapient.hms.domain.InterviewDetailsService;
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(InterviewDetailsService)
+class InterviewDetailsServiceTests {
+	
+	//def interviewDetailsService
+	void testSample(){
+	
+		assertEquals(service.getAllInterviewers(), 0)
+		
+		
+	}
+
+    
+}
