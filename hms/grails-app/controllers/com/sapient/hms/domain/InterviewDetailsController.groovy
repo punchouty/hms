76fdc031014befcb0284f976a0cca4904f994400
aku211 <aku211@dluaku2113762.sapient.com>
2014-06-25 10:06:12 +0530

    updated

---
+++ hms/grails-app/controllers/com/sapient/hms/domain/InterviewDetailsController.groovy
@@ -0,0 +1,35 @@
+package com.sapient.hms.domain
+
+import grails.converters.JSON
+
+class InterviewDetailsController {
+	
+	
+	def interviewDetailsService
+		
+	
+	def getPositions(Integer max) {
+		def positions = interviewDetailsService.allPositions()
+		render positions as JSON
+	}
+	
+	def getInterviewers(){
+		
+		List<User> interviwers=interviewDetailsService.allInterviewers();
+		render interviwers as JSON
+	}
+	
+	def getRounds(){
+		
+		def rounds=interviewDetailsService.allRounds()
+		render rounds as JSON
+	}
+	
+	def getHiringProcess(){
+		
+		def hiringProcess=interviewDetailsService.allHiringProcesses()
+		render hiringProcess as JSON
+	}
+
+    def index() { }
+}
