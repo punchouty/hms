904b1fc7465f1fb40a7201a9cc3685de5c4b60c3
priyasharma2702 <psharma71@sapient.com>
2014-06-27 13:44:30 +0530

    Update hiringController.js

--- hms/web-app/js/hms/hiringController.js
+++ hms/web-app/js/hms/hiringController.js
@@ -2,11 +2,51 @@
 
 /* Controllers */
 
-angular.module('hmsApp.controllers', [])
-  .controller('hiringController', ['$scope', function($scope) {
+'use strict';
 
+/* Controllers */
+
+angular.module('myApp.controllers', [])
+  .controller('scheduleViewCtrl', ['$scope', function($scope,$routeParams,scheduledInterviews) {
+  $scope.name = 'scheduleViewCtrl';
+  scheduledInterviews.getInterviews().then(function(interviews) {
+    $scope.interviews = interviews;
+  });
+ 
+  $scope.$on('handleScheduledInterviews', function(events, interviews) {
+    $scope.interviews = interviews;
+  });
+  $scope.addCandidate = function() {
+ ; }
+  /* var interview= {
+     position: $scope.position,
+	 workflow: $scope.workflow,
+	 round: $scope.round,
+	 candidate: $scope.candidate,
+	 interviewer: $scope.interviewer
+	 };
+    $scope.interviews.push(interview); */
+  
+  //  handling the submit button for the form
+  $scope.schedule = function(interview) {
+    $params = $.param({
+     "position": interview.position,
+	 "workflow": interview.workflow,
+	 "round": interview.round,
+	 "candidate": interview.candidate,
+	 "interviewer": interview.interviewer,
+	 "time":interview.time
+    })
+	scheduledInterviews.saveInterviews($params);
+	}
   }])
-  .controller('assessmentController', ['$scope', function($scope) {
+  .controller('MyCtrl2', ['$scope', function($scope) {
 
   }])
   ;
+
+  
+  
+  
+  
+ 
