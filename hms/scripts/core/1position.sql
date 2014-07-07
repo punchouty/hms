3e4d3c4acc9cdc6f492e0b864218905c02e738c8
SK <sk@gmail.com>
2014-07-07 12:08:34 +0530

    Add

---
+++ hms/scripts/core/1position.sql
@@ -0,0 +1,9 @@
+/*
+-- Query: SELECT * FROM hms.position
+LIMIT 0, 1000
+
+-- Date: 2014-07-02 11:19
+*/
+use hms;
+INSERT INTO `position` (`id`,`version`,`date_created`,`last_updated`,`name`) VALUES (1,0,'2012-12-12 00:00:00','2012-12-12 00:00:00','Associate');
+INSERT INTO `position` (`id`,`version`,`date_created`,`last_updated`,`name`) VALUES (2,0,'2012-12-12 00:00:00','2012-12-12 00:00:00','Manager');
---
+++ hms/scripts/core/2candidate_details.sql
@@ -0,0 +1,12 @@
+/*
+-- Query: SELECT * FROM hms.candidate_detail
+LIMIT 0, 1000
+
+-- Date: 2014-07-04 12:37
+*/
+INSERT INTO `candidate_detail` (`id`,`version`,`date_created`,`last_updated`,`location`,`name`) VALUES (1,0,'2012-12-12 00:00:00','2012-12-12 00:00:00','9927128290','Sumit');
+INSERT INTO `candidate_detail` (`id`,`version`,`date_created`,`last_updated`,`location`,`name`) VALUES (2,0,'2012-12-12 00:00:00','2012-12-12 00:00:00','9927128291','Puneet');
+INSERT INTO `candidate_detail` (`id`,`version`,`date_created`,`last_updated`,`location`,`name`) VALUES (3,0,'2014-07-03 15:15:46','2014-07-03 15:15:46','9927128292','Anil');
+INSERT INTO `candidate_detail` (`id`,`version`,`date_created`,`last_updated`,`location`,`name`) VALUES (4,0,'2014-07-04 12:10:58','2014-07-04 12:10:58','9927128293','Anil');
+INSERT INTO `candidate_detail` (`id`,`version`,`date_created`,`last_updated`,`location`,`name`) VALUES (5,0,'2014-07-04 12:19:15','2014-07-04 12:19:15','9927128294','Sunil');
+INSERT INTO `candidate_detail` (`id`,`version`,`date_created`,`last_updated`,`location`,`name`) VALUES (6,0,'2014-07-04 12:25:43','2014-07-04 12:25:43','9927128290','Deepti');
