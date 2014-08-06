c34df60275c89f2ac4e466f62d070d0940ae9bd3
kmahaw <kmahaw@dlukmahaw95062.sapient.com>
2014-08-06 16:59:14 +0530

    updated

--- hms/grails-app/controllers/com/sapient/hms/controllers/CandidateDetailsController.groovy
+++ hms/grails-app/controllers/com/sapient/hms/controllers/CandidateDetailsController.groovy
@@ -45,15 +45,6 @@ class CandidateDetailsController {
 			render CandidateDetailInstance.list as JSON
 		}
 	}
-	
-	def search(){
-		
-		def result=request.JSON
-		if(result.name){
-			def CandidateDetailInstance=CandidateDetail.findAllByName(result.name)
-			render CandidateDetailInstance.list as JSON
-		}
-	}
 
 	//    def show(Long id) {
 	//        def candidateDetailsInstance = CandidateDetail.get(id)
